b5c50c7052504b5fd37667a8a5d23dcb27e4ccf7
Marcus Stevenson <marcusstevenson1007@gmail.com>
2020-11-10 17:38:25 -0700

    Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -8,3 +8,46 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 //TODO
+import {foods} from './foods'
+
+let fetaured_food = document.querySelector('.featured');
+for(let f of foods){
+    let food_thumb = document.getElementById('f' + f.id)
+    food_thumb.innerHTML = `
+        <img src="${f.poster}">
+    `
+
+    food_thumb.onclick = function(){
+        selectFood(f)
+    }
+}
+
+function selectFood(f){
+    fetaured_food.innerHTML = `
+    <img src="${f.poster}">
+    <h1>${f.title}</h1>
+    <p>${f.plot}</p> `
+}
+
+/*function searchFoods(event){
+    if(event) {
+        event.preventDefault()
+    }
+
+    let input = document.querySelector('[type="search"]').value || ""
+    for( let f of foods){
+        let food_thumb = document.getElementById('f' + f.id)
+
+        if(f.title.toUpperCase().indexOf(input.toUpperCase()) == -1){
+            //foods.thumb.classList.add('hidden')
+            food_thumb.style.display = 'none'
+        } else {
+            //foods.thumb.classList.remove('hidden')
+            food_thumb.style.display = 'block'
+
+        }
+    }
+}
+document.querySelector('button').onclick = searchFoods
+document.querySelector('[type="search"]').onsearch = searchFood //search on enter key
+document.forms[0].addEventListener('submit', searchFoods, false)*/
