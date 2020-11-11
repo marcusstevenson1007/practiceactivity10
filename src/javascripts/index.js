ebd6017131c7fa2155b4eff16e9e73acad1aed82
Marcus Stevenson <marcusstevenson1007@gmail.com>
2020-11-10 18:24:52 -0700

    final submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -28,26 +28,3 @@ function selectFood(f){
     <h1>${f.title}</h1>
     <p>${f.plot}</p> `
 }
-
-/*function searchFoods(event){
-    if(event) {
-        event.preventDefault()
-    }
-
-    let input = document.querySelector('[type="search"]').value || ""
-    for( let f of foods){
-        let food_thumb = document.getElementById('f' + f.id)
-
-        if(f.title.toUpperCase().indexOf(input.toUpperCase()) == -1){
-            //foods.thumb.classList.add('hidden')
-            food_thumb.style.display = 'none'
-        } else {
-            //foods.thumb.classList.remove('hidden')
-            food_thumb.style.display = 'block'
-
-        }
-    }
-}
-document.querySelector('button').onclick = searchFoods
-document.querySelector('[type="search"]').onsearch = searchFood //search on enter key
-document.forms[0].addEventListener('submit', searchFoods, false)*/
